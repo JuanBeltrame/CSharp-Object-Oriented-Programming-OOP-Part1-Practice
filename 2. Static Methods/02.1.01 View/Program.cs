463742b128cbf14773f685c63225de3ba2d8177a
Juan Beltrame <juan_beltrame@hotmail.com>
2025-01-15 13:43:58 -0300

    Update

--- 2. Static Methods/02.1.01 View/Program.cs
+++ 2. Static Methods/02.1.01 View/Program.cs
@@ -1,2 +1,5 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/* 
+Excercise Link: https://codeutnfra.github.io/programacion_2_laboratorio_2_apuntes/docs/clases/estaticos/Ejercicios/I01-validador-rangos/
+*/
+
+//--Programa Principal---
