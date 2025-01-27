c52c92672809f7832be8464095ca294d0bbe3f57
Juan Beltrame <juan_beltrame@hotmail.com>
2025-01-27 05:20:42 -0300

    feat: Add Validador class and update project references
    - Introduced `Validador` class with static method
      `ValidarRespuesta` for input validation.
    - Removed `Class1` class to simplify the library.
    - Updated `02.1.02 View.csproj` to reference the new
      library for functionality access.
    - Enhanced `Program.cs` to include user input loop
      for integer accumulation and continuation prompt.
    - Modified solution file to reflect new project
      references and configurations.

---
+++ 2. Static Methods/02.1.02 Library/Validador.cs
@@ -0,0 +1,12 @@
+﻿namespace _02._1._02_Library
+{
+    public class Validador
+    {
+        public static bool ValidarRespuesta(char value) => value switch
+        {
+            's' => true,
+            'n' => false,
+            _ => false
+        };
+    }
+}
--- 2. Static Methods/02.1.02 View/Program.cs
+++ 2. Static Methods/02.1.02 View/Program.cs
@@ -1,3 +1,25 @@
-﻿/* 
+﻿using _02._1._02_Library;
+/* 
 Excercise Link: https://codeutnfra.github.io/programacion_2_laboratorio_2_apuntes/docs/clases/estaticos/Ejercicios/I02-desea-continuar/
 */
+
+int acumulator = 0;
+int number;
+char desicion;
+
+Console.Write($"Ingresar un numero entero: ");
+number = int.Parse(Console.ReadLine()!);
+acumulator += number;
+
+Console.WriteLine("Desea Continuar? (S/N): ");
+desicion = char.Parse(Console.ReadLine()!);
+
+while (Validador.ValidarRespuesta(desicion))
+{
+    Console.Write($"Ingresar un numero entero: ");
+    number = int.Parse(Console.ReadLine()!);
+    acumulator += number;
+
+    desicion = char.Parse(Console.ReadLine()!);
+    Console.WriteLine("Desea Continuar? (S/N): " );
+}
